e1787ceaf729c7b4bb196c1cb44f10cfed971430
Kathleen Kamali <kathleenkamali@gmail.com>
2019-07-29 17:27:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
@@ -5,3 +6,12 @@ end
 def introduction_with_language(name, language) 
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+>>>>>>> 847de5df6da31a9da2091bf8449aa65ff2700089
